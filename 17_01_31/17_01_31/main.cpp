ab10c3361e81d4ae69ab3ca03d4ea9000fa2b5f1
Joong Oh Ban <rainyban93@gmail.com>
2017-02-01 17:59:57 +0900

    C final

--- 17_01_31/17_01_31/main.cpp
+++ 17_01_31/17_01_31/main.cpp
@@ -129,18 +129,116 @@ puts() : 표준 출력, fputs() : 지정한 스트림
 /////////////////////////linked list로 파일 읽고 쓰기
 ///////////////////////// 1. 맨뒤 추가, 2. 수정, 3. 삭제, 4. 종료
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/////////////////////////linked list로 파일 읽고 쓰기
+///////////////////////// 1. 맨뒤 추가, 2. 수정, 3. 삭제, 4. 종료
+
+/*
+typedef struct one
+{
+char st[255];
+int num;
+struct one* link;
+}TEXT;
+
+TEXT* insert(TEXT* pastptr, int* num);
+void display(TEXT* pastptr, int* num);
+
+void main()
+{
+int keyin=0;
+int count=0;
+TEXT* temp = NULL;
+FILE *fp1 = NULL;
+FILE *fp2 = NULL;
+
+fp2 = fopen("../17_01_31/a.txt", "rt");
+
+
+//   while (!feof(fp2))
+//   {
+//      fgets(buf, 255, fp2);
+//      line++;
+//   }
+//   printf("라인 수 : %d", line);
+
+//   TEXT *ptr = (TEXT*)malloc(sizeof(TEXT)*line);
+
+//   for (int count=0; count<line; ++count)
+//   {
+//      fgets(buf, 255, fp2);
+//      strcpy(ptr[count].st, buf);
+//      if (flag!=line)
+//      {
+//         ptr[count].link1 = &ptr[count + 1];
+//         flag++;
+//      }
+//   }
+
+while(keyin!=3)
+{
+printf("메뉴를 선택하거라\n1. 맨 뒤 추가  2. 수정  3. 삭제  4. 종료 : ");
+scanf("%d",&keyin);
+switch(keyin)
+{
+case 1 :   temp = insert(temp, &count);
+break;
+case 2 :   display(temp, &count);
+break;
+case 3 :   printf("종료합니다 ^^.\n");
+break;
+default :   printf("똑바로 입력하거라.!\n");
+break;
+}
+}
+}
+
+TEXT* insert(TEXT* pastptr, int* num)
+{
+TEXT* ptr;
+printf("%d번째 입력입니다.\n",(*num)+1);
+ptr = (TEXT*)malloc(sizeof(TEXT));
+printf("%d번째 문자열 : ",(*num)+1);
+scanf("%s",ptr->st);         //heap의 주소를 가리키기 때문에 간접접근연산자 사용
+printf("%d번째 라인 : ",(*num)+1);
+ptr->num = (*num)+1;
+ptr->link = pastptr;
+(*num)++;
+return ptr;   //ptr자체는 heap의 시작주소를 가리킴
+}
+
+void display(TEXT* pastptr, int* num)
+{
+int count;
+printf("\tdisplay함수 진입 \n");
+for(count = 0; count<(*num); ++count)
+{
+printf("%d번째 문자열 : %s\t",(*num)-count,pastptr->st);
+printf("%d번째 라인 : %d\t\t",(*num)-count,pastptr->num);
+printf("\n");
+pastptr = pastptr->link;
+}
+}
+
+*/
+
+
 typedef struct one
 {
 	char st[255];
 	struct one* link1;
 }TEXT;
 
-void add(TEXT *Aptr, const char* Ach, int *Aline);
+TEXT* add(TEXT *Aptr, const char* Ach, int *Aline);
 void modify(TEXT *Mptr, const char* Mch);
 void junk(TEXT *Jptr1, TEXT *Jptr2, TEXT *Jptr3, int Jnum);
+int linecount();
 void main()
 {
-	int ch,select,temp;
+	int ch, select, temp;
 	int line = 0;
 	char flag = 1;
 	char buf[255];
@@ -148,58 +246,52 @@ void main()
 	FILE *fp2 = NULL;
 	TEXT *start = NULL;
 	TEXT *past = NULL;
+	TEXT *Slink = NULL;
 
-	fp2 = fopen("../17_01_31/a.txt", "rt");
-	
+	line = linecount();
+	TEXT *ptr = (TEXT*)malloc(sizeof(TEXT)*line);
+
+	fp2 = fopen("../17_01_31/a.txt", "r");
 	if (fp2 == NULL)
 	{
 		printf("file open error");
 		exit(-1);
 	}
 
-	while (!feof(fp2))
-	{
-		fgets(buf, 255, fp2);
-		line++;
-	}
-	printf("라인 수 : %d", line);
-
-	TEXT *ptr = (TEXT*)malloc(sizeof(TEXT)*line);
-
-	for (int count=0; count<line; ++count)
+	start = &ptr[0];   // 시작 주소
+	past = &ptr[line - 1];// 맨 마지막 주소
+	for (int count = 0; count<line; ++count)
 	{
 		fgets(buf, 255, fp2);
 		strcpy(ptr[count].st, buf);
-		if (flag!=line)
-		{
+		if (count < line - 1)
 			ptr[count].link1 = &ptr[count + 1];
-			flag++;
-		}
 	}
+	flag = line;
 
 	printf("메뉴를 선택하거라\n1. 맨 뒤 추가  2. 수정  3. 삭제  4. 종료 : ");
 	scanf("%d", &select);
-	
-	while(select != 4)
+
+	while (select != 4)
 	{
 		switch (select)
 		{
 		case 1:
 			printf("맨 뒤에 추가할 문자열 입력 : ");
 			scanf("%s", buf);
-			add(&ptr[line-1], buf, &line);
+			past = add(past, buf, &line);
 			break;
 		case 2:
 			printf("수정할 위치 : ");
 			scanf("%d", &temp);
 			printf("수정할 문자열 : ");
 			scanf("%s", buf);
-			modify(&ptr[temp-1], buf);
+			modify(&ptr[temp - 1], buf);
 			break;
 		case 3:
 			printf("삭제할 위치 : ");
 			scanf("%d", &temp);
-			junk(&ptr[temp-1], &ptr[temp-2], &ptr[temp], temp);
+			junk(&ptr[temp - 1], &ptr[temp - 2], &ptr[temp], temp);
 			break;
 		case 4:
 			printf("종료합니다.\n");
@@ -211,33 +303,27 @@ void main()
 		printf("메뉴를 선택하거라\n1. 맨 뒤 추가  2. 수정  3. 삭제  4. 종료 : ");
 		scanf("%d", &select);
 	}
-	/*
-	while ((ch = getchar()) != '\n')
-	{
-		fputc(ch, fp2);			// ../17_01_31/a.txt 에 입력한 내용으로 생성
-	}
 
-	fp1 = fopen("../17_01_31/a.txt", "r");
-
-	if (fp1 == NULL)
+	fp1 = fopen("../17_01_31/a.txt", "w");
+	Slink = start;
+	for (int count = 0; count < line; ++line)
 	{
-		printf("files open error\n");
-		exit(-1);
+		fprintf(fp1, Slink->st);
+		printf("test : %s", Slink->st);
+		if (count + 1 == line)
+			break;
+		Slink = Slink->link1;
 	}
-
-	while ((ch = fgetc(fp1)) != EOF)	// EOF는 -1을 의미하며 파일의 끝
-	{
-		putc(ch, stdout);	// 문자를 하나씩 읽어서 콘솔에 출력
-	}
-	*/
+	free (ptr);
 }
-
-void add(TEXT *Aptr, const char* Ach, int *Aline)
+ 
+TEXT* add(TEXT *Aptr, const char* Ach, int *Aline)
 {
 	TEXT *temp = (TEXT*)malloc(sizeof(TEXT));
 	strcpy(temp->st, Ach);
 	*Aline++;
 	Aptr->link1 = temp;
+	return temp;
 }
 void modify(TEXT *Mptr, const char* Mch)
 {
@@ -249,7 +335,26 @@ void junk(TEXT *Jptr1, TEXT *Jptr2, TEXT *Jptr3, int Jnum)
 	delete Jptr2;
 }
 
+int linecount()
+{
+	int line = 0;
+	char buf[255];
+	FILE *fp = NULL;
+	fp = fopen("../17_01_31/a.txt", "rt");
 
+	if (fp == NULL)
+	{
+		printf("file open error");
+		exit(-1);
+	}
 
+	while (!feof(fp))
+	{
+		fgets(buf, 255, fp);
+		line++;
+	}
+	fclose(fp);
+	printf("라인 수 : %d\n", line);
 
-
+	return line;
+}
